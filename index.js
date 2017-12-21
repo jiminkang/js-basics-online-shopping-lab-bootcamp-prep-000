3e839b3707949be58d947abbf688f08be0d3cd49
Jimin Kang <jimin.kang91@gmail.com>
2017-12-21 00:18:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,21 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 100);
+ var cartObj = { [item] : price};
+ cart.push(cartObj);
+ console.log ("${item} has been added to your cart.");
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  for (var i=0; i<cart.length;i++){
+    if(car.length==1) console.log ("In your cart you have ${Object.keys(cart[i])[0]} at ${cart[i][Object.keys(cart[i])[0]}.");
+    else if (cart.length ==2){
+      
+    }
+  }
 }
 
 function total() {
