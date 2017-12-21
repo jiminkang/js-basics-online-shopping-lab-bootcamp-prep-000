568d20ca29cfa1f9b041241e3e33d003d53f91cc
Jimin Kang <jimin.kang91@gmail.com>
2017-12-21 01:22:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,4 +71,13 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber == undefined){
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
+  else{
+    var totalPrice=total();
+    console.log(`Your total cost is ${totalPrice}, which will be charged to the card ${cardNumber}`);
+    cart =[];
+    return cart;
+  }
 }
